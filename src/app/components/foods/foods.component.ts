9ed27717ad5ac52d75224e762ab73b148c03e629
unknown <arth.nepomuceno@gmail.com>
2023-08-10 15:02:49 -0300

    :sparkles: Feat: list of foods when typing to select food

--- src/app/components/foods/foods.component.ts
+++ src/app/components/foods/foods.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
-import { TACO } from 'src/app/database/taco';
-import { Food } from 'src/app/interfaces/food';
+import { Component, OnInit } from '@angular/core';
+import { Food } from 'src/app/interfaces/food.interface';
+import { FoodService } from 'src/app/services/food-services/food.service';
+import { Observable, Subject, debounceTime, distinctUntilChanged, switchMap } from 'rxjs';
 
 
 
@@ -9,11 +10,37 @@ import { Food } from 'src/app/interfaces/food';
   templateUrl: './foods.component.html',
   styleUrls: ['./foods.component.css']
 })
-export class FoodsComponent {
-  tacoTable = TACO;
+export class FoodsComponent implements OnInit {
+  //ATRIBUTOS
+  tacoTable: Food[] = [];
+
   selectedFood?: Food;
+
+  foodMenu$!: Observable<Food[]>;
+  
+  private searchTerms = new Subject<string>();
+
+  //CONSTRUTOR
+  constructor(
+    private foodService: FoodService
+  ){}
+
+  //MÉTODOS
+  ngOnInit(): void {
+    this.foodService.getTacoTable().subscribe(data => this.tacoTable = data);
+
+    this.foodMenu$ = this.searchTerms.pipe(
+      debounceTime(300),
+      distinctUntilChanged(),
+      switchMap((term: string) => this.foodService.searchFood(term))
+    )
+  }
 
   onSelect(element: Food){
     this.selectedFood = element;
+  }
+
+  onSearch(term: string): void{
+    this.searchTerms.next(term);
   }
 }
